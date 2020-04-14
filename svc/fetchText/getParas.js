385f6a0c900e7a3ba8079180a4f5bc5ac773675d
aschmach <anders.schmachtenberg@gmail.com>
2020-04-14 22:01:00 +1000

    reverse paragraph array to get paragraphs correct in the beginning

--- svc/fetchText/getParas.js
+++ svc/fetchText/getParas.js
@@ -1,38 +1,39 @@
 module.exports = getParas;
 
 function getParas(data, identifier_words = {}) {
-  // console.log({ identifier_words });
+  console.log({ identifier_words });
   let fragments = [];
   const textStartIndicatorsArray = ["<p", "<h"];
   const textStartIndicators = new RegExp(textStartIndicatorsArray.join("|"));
   const textEndIndicatorsArray = ["</p", "</h"];
   const textEndIndicators = new RegExp(textEndIndicatorsArray.join("|"));
   const headlines = getHeadlines(data, textEndIndicators);
-  let paras = data.split(textStartIndicators);
+  let items = data.split(textStartIndicators);
 
-  paras.shift();
-  let triggeredStart = false;
+  items.shift();
+  items.reverse();
   let triggeredEnd = false;
+  let triggeredStart = false;
 
-  paras = paras.reduce((array, item, i, arr) => {
+  let paras = items.reduce((array, item, i, arr) => {
     item = item.split(textEndIndicators)[0];
     item = stripHtmlTags(item);
-    if (isFinishingPara(item)) {
-      console.log("is finishing para");
-      triggeredEnd = true;
-      arr.splice(1); // ejects early
-    }
-    if (!triggeredStart) {
-      if (item.includes(identifier_words.start_words)) {
-        triggeredStart = true;
+    // if (isFinishingPara(item)) {
+    //   console.log("is finishing para");
+    //   triggeredStart = true;
+    //   arr.splice(1); // ejects early
+    // }
+    if (!triggeredEnd) {
+      if (item.includes(identifier_words.end_words)) {
+        triggeredEnd = true;
       }
     }
 
-    if (triggeredStart && !triggeredEnd) {
+    if (triggeredEnd && !triggeredStart) {
       const { includePara, fragment } = includeParaFunc(item, data);
       if (includePara) {
         if (headlines.indexOf(item) > -1) {
-          item = `<h4>${item}</h4>`;
+          item = `<b>${item}`;
         }
         array.push(item);
         fragments.push(fragment);
@@ -41,14 +42,17 @@ function getParas(data, identifier_words = {}) {
       }
     }
 
-    if (!triggeredEnd) {
-      if (item.includes(identifier_words.end_words)) {
-        triggeredEnd = true;
+    if (!triggeredStart) {
+      if (item.includes(identifier_words.start_words)) {
+        triggeredStart = true;
         arr.splice(1); // ejects early
       }
     }
     return array;
   }, []);
+
+  paras.reverse();
+  console.log({ paras });
 
   return { paras, fragments };
 }
@@ -129,7 +133,16 @@ function includeParaFunc(para, data) {
   const includePara =
     sufficientLength &&
     !isExcludedPara &&
+    !isBogusPara(para) &&
     (fragmentIncluded || data.includes(para));
 
   return { includePara, fragment };
+}
+
+function isBogusPara(item) {
+  if (item.split("\n").length > 3) {
+    return true;
+  } else {
+    return false;
+  }
 }
